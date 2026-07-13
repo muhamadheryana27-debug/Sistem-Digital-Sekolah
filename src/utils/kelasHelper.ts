b64bd495bf8db01fcbfb7268c9e7eda502da87f5
muhamadheryana27-debug <muhamadheryana27@guru.smp.belajar.id>
2026-07-13 13:08:16 +0700

    Create kelasHelper.ts

---
+++ src/utils/kelasHelper.ts
@@ -0,0 +1,8 @@
+export const LIST_KELAS_LENGKAP = [
+  // Kelas VII
+  "VII-A", "VII-B", "VII-C", "VII-D", "VII-E", "VII-F", "VII-G", "VII-H",
+  // Kelas VIII
+  "VIII-A", "VIII-B", "VIII-C", "VIII-D", "VIII-E", "VIII-F", "VIII-G", "VIII-H",
+  // Kelas IX
+  "IX-A", "IX-B", "IX-C", "IX-D", "IX-E", "IX-F", "IX-G", "IX-H"
+];
